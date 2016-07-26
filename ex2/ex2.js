87a6700b0cd5e87f29c19c93c087ed2252000b7b
Eubene Sa <eubene.sa@gmail.com>
2016-07-26 11:35:07 -0700

    Finish ex2

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -1,12 +1,14 @@
-function foo() { }
+function foo(x, y, z, ...rest) {
+  return [x, ...rest];
+}
 
 function bar() {
-	var a1 = [ 2, 4 ];
-	var a2 = [ 6, 8, 10, 12 ];
+  var a1 = [2, 4];
+  var a2 = [6, 8, 10, 12];
 
-	return foo();
+  return foo(...a1, ...a2);
 }
 
 console.log(
-	bar().join("") === "281012"
+  bar().join("") === "281012"
 );
